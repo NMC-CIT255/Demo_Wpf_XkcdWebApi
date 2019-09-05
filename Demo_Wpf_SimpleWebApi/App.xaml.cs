95e2999b3eb96fd45ab5cae8c6c5789d9e60a26a
johnvelis <john@johnvelis.com>
2019-09-05 11:30:48 -0400

    Initial Push

--- Demo_Wpf_SimpleWebApi/App.xaml.cs
+++ Demo_Wpf_SimpleWebApi/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Demo_Wpf_SimpleWebApi.BusinessLayer;
 
 namespace Demo_Wpf_SimpleWebApi
 {
@@ -13,5 +14,9 @@ namespace Demo_Wpf_SimpleWebApi
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            AppBusiness appBusiness = new AppBusiness();
+        }
     }
 }
